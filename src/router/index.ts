3a7c9910eeed2d1e8935d164f82791ef2980e27b
JEFF OKOLI <jahbuchimokoli3@gmail.com>
2023-08-24 17:14:01 +0100

    project setup in progress

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+// @ts-ignore
+import Editor from '@/views/Editor.vue'
+// @ts-ignore
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Editor',
+      component: Editor,
+      meta:{
+        hasAccess:true
+      }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/profile',
+      name: 'Profile Details',
+      // @ts-ignore
+      component: () => import('../views/ProfileDetails.vue'),
+      meta:{
+        hasAccess:true
+      }
+    },
+    {
+      path: '/preview',
+      name: 'Preview Page',
+      // @ts-ignore
+      component: () => import('../views/Preview.vue'),
+      meta:{
+        hasAccess:true
+      }
+    },
   ]
 })
 
